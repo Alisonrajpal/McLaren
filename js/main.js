a100b7a36aaa772ce27b817c64c91efdf7f727ee
Alisonrajpal <alisonrajpal@gmail.com>
2024-08-28 14:33:46 +0200

    Fixed minor errors with the social icons and still working on the nav

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function toggleMenu() {
 }
 
 //Change the background video when click on the gallery image
-function chnageVideo(name) {
+function changeVideo(name) {
   const byVideoList = document.querySelectorAll(".bg-video");
   const trailers = document.querySelectorAll(".trailer");
   const models = document.querySelectorAll(".model");
@@ -20,4 +20,46 @@ function chnageVideo(name) {
       video.classList.add("active");
     }
   });
+
+  //mapping model name change
+  models.forEach((model) => {
+    model.classList.remove("active");
+    if (model.classList.contains(name)) {
+      model.classList.add("active");
+    }
+  });
+
+  //mapping trailers
+  trailers.forEach((video) => {
+    video.classList.remove("active");
+    if (video.classList.contains(name)) {
+      video.classList.add("active");
+    }
+  });
+}
+
+//change the play and pause button on click event
+function toggleplay() {
+  const play = document.querySelector(".play");
+  const pause = document.querySelector(".pause");
+  play.classist.toggle("active");
+  pause.classList.toggle("active");
+}
+
+////Video Play and Pause
+function pauseVideo() {
+  const byVideoList = document.querySelectorAll(".bg-video");
+  bgVideoList.forEach((video) => {
+    video.pause();
+  });
+
+  toggleplay();
+}
+
+function playVideo() {
+  const byVideoList = document.querySelectorAll(".bg-video");
+  bgVideoList.forEach((video) => {
+    video.play();
+  });
+  toggleplay();
 }
